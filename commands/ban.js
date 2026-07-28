9d573941379450c1d1ae48bb2a97453a74d47be6
urdushahzaib111-ctrl <urdushahzaib111@gmail.com>
2026-07-28 12:07:31 +0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,15 +3,16 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ban')
-        .setDescription('Bans a member from the server.')
-        .addUserOption(option => 
+        .setDescription('Permanently bans a member from the server.')
+        .addUserOption(option =>
             option.setName('target')
                 .setDescription('The member to ban')
                 .setRequired(true))
-        .addStringOption(option => 
+        .addStringOption(option =>
             option.setName('reason')
                 .setDescription('Reason for banning'))
         .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
     async execute(interaction) {
         const target = interaction.options.getMember('target');
         const reason = interaction.options.getString('reason') ?? 'No reason provided';
@@ -22,10 +23,10 @@ module.exports = {
 
         try {
             await target.ban({ reason });
-            await interaction.reply(`Successfully banned ${target.user.tag} for: ${reason}`);
+            await interaction.reply({ content: `Successfully banned ${target.user.tag}. Reason: ${reason}` });
         } catch (error) {
             console.error(error);
-            await interaction.reply({ content: 'There was an error trying to ban this user!', ephemeral: true });
+            await interaction.reply({ content: 'There was an error trying to ban this user! Check role hierarchy permissions.', ephemeral: true });
         }
     },
 };
