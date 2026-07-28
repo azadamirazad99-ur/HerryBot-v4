9a12987b4ea0fc43871c2d76b7511b3b86aa1504
urdushahzaib111-ctrl <urdushahzaib111@gmail.com>
2026-07-28 11:12:58 +0500

    Create roll.js

---
+++ commands/roll.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('roll')
+        .setDescription('Rolls a 6-sided dice.'),
+    async execute(interaction) {
+        const roll = Math.floor(Math.random() * 6) + 1;
+        await interaction.reply(`🎲 You rolled a: **${roll}**`);
+    },
+};
