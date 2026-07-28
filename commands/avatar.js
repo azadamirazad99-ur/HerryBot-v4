d5456968a89884bfa570e7a6b6280a693dd51d5c
urdushahzaib111-ctrl <urdushahzaib111@gmail.com>
2026-07-28 11:13:57 +0500

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Displays a user avatar.')
+        .addUserOption(option =>
+            option.setName('target')
+                .setDescription('The user')
+                .setRequired(false)),
+    async execute(interaction) {
+        const target = interaction.options.getUser('target') || interaction.user;
+        const embed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle(`${target.tag}'s Avatar`)
+            .setImage(target.displayAvatarURL({ size: 1024, dynamic: true }))
+            .setTimestamp();
+        await interaction.reply({ embeds: [embed] });
+    },
+};
