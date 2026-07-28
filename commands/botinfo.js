7ff37b7486c22582e072522cc3b4ac0a5ba87ec4
urdushahzaib111-ctrl <urdushahzaib111@gmail.com>
2026-07-28 11:10:33 +0500

    Create botinfo.js

---
+++ commands/botinfo.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('botinfo')
+        .setDescription('Shows information about the bot.'),
+    async execute(interaction) {
+        const embed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle('HerryBot Info')
+            .setDescription('Your custom Discord bot running smoothly!')
+            .addFields(
+                { name: 'Status', value: 'Online & Active', inline: true },
+                { name: 'Platform', value: 'Railway', inline: true }
+            )
+            .setTimestamp();
+        await interaction.reply({ embeds: [embed] });
+    },
+};
+
