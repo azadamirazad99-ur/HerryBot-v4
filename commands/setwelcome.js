39fed78f6191b6413af1bffa844573503a871960
urdushahzaib111-ctrl <urdushahzaib111@gmail.com>
2026-07-28 11:52:22 +0500

    Create setwelcome.js

---
+++ commands/setwelcome.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('setwelcome')
+        .setDescription('Sets the official channel for welcoming new members.')
+        .addChannelOption(o => o.setName('channel').setDescription('Select welcome text channel').setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction) {
+        const targetChannel = interaction.options.getChannel('channel');
+        
+        // Yahan aap channel ID ko database ya file mein save kar sakte ho
+        await interaction.reply({ 
+            content: `✅ Success! Welcome channel has been set to ${targetChannel}. Ab se naye members yahin welcome honge!`, 
+            ephemeral: true 
+        });
+    },
+};
+
