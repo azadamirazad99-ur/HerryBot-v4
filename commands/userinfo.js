590b0e20c5a6f8845c03bcde29b3e666414ad95e
urdushahzaib111-ctrl <urdushahzaib111@gmail.com>
2026-07-28 10:54:11 +0500

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('userinfo')
+        .setDescription('Shows information about a user.')
+        .addUserOption(option =>
+            option.setName('target')
+                .setDescription('The user to get info about')
+                .setRequired(false)),
+    async execute(interaction) {
+        const target = interaction.options.getMember('target') || interaction.member;
+        const user = target.user;
+
+        const embed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle(`User Info - ${user.tag}`)
+            .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+            .addFields(
+                { name: 'Username', value: user.tag, inline: true },
+                { name: 'ID', value: user.id, inline: true },
+                { name: 'Joined Server', value: `<t:${Math.floor(target.joinedTimestamp / 1000)}:R>`, inline: false },
+                { name: 'Account Created', value: `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`, inline: false }
+            )
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
